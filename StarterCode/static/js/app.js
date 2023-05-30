d3082653e5bf8b668394d5253927e8cf00e27ac9
Shakhnoza Vafaeva <svafaeva93@gmail.com>
2023-05-30 15:38:17 -0400

    Adding changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -29,16 +29,16 @@ function init() {
     });
 
     //  Set first sample from the list 
-        let sample_one = names[0];
+        let sample_i = sampleNames[0];
 
          // Log the value of sample_one
-         console.log(sample_one);
+         console.log(sample_i);
  
          // Build the initial plots
-         buildMetadata(sample_one);
-         buildBarChart(sample_one);
-         buildBubbleChart(sample_one);
-         buildGaugeChart(sample_one);
+         buildMetadata(sample_i);
+         buildBarChart(sample_i);
+         buildBubbleChart(sample_i);
+         buildGaugeChart(sample_i);
  
      });
  };
@@ -52,20 +52,20 @@ function buildMetadata(sample) {
       // Retrieve all metadata
       let metadata = data.metadata;
 
-      // Filter based on the value of the sample
-      let value = metadata.filter(result => result.id == sample);
+      // Filter data where id is equal to the sample 
+      let filtered_data = metadata.filter(result => result.id == sample);
 
-      // Log the array of metadata objects after the have been filtered
-      console.log(value)
+      // Log the array of metadata objects after they have been filtered
+      console.log(filtered_data)
 
       // Get the first index from the array
-      let valueData = value[0];
+      let selected_value = filtered_data[0];
 
       // Clear out metadata
       d3.select("#sample-metadata").html("");
 
       // Use Object.entries to add each key/value pair to the panel
-      Object.entries(valueData).forEach(([key,value]) => {
+      Object.entries(selected_value).forEach(([key,value]) => {
 
           // Log the individual key/value pairs as they are being appended to the metadata panel
           console.log(key,value);
@@ -85,10 +85,10 @@ function buildBarChart(sample) {
       let sampleData = data.samples;
 
       // Filter based on value of the sample
-      let value = sampleData.filter(result => result.id == sample);
+      let filtered_data = sampleData.filter(result => result.id == sample);
 
       // Get the first index from the array
-      let selected_value = value[0];
+      let selected_value = filtered_data[0];
 
       // Get the otu_ids, lables, and sample values
       let otu_ids = selected_value.otu_ids;
@@ -132,28 +132,20 @@ function buildBubbleChart(sample) {
     let sampleData = data.samples;
 
     // Filter based on value of the sample
-    let value = sampleData.filter(result => result.id == sample);
+    let filtered_data = sampleData.filter(result => result.id == sample);
 
     // Get the first index from the array
-    let selected_value = value[0];
+    let selected_value = filtered_data[0];
 
-    // Get the otu_ids, lables, and sample values
-    let otu_ids = selected_value.otu_ids;
-    let otu_labels = selected_value.otu_labels;
-    let sample_values = selected_value.sample_values;
-
-    // Log the data to the console
-    console.log(otu_ids,otu_labels,sample_values);
-      
     // Set up the trace for the bubble chart
       let trace1 = {
-          x: otu_ids,
-          y: sample_values,
-          text: otu_labels,
+          x: selected_value.otu_ids,
+          y: selected_value.sample_values,
+          text: selected_value.otu_labels,
           mode: "markers",
           marker: {
-              size: sample_values,
-              color: otu_ids,
+              size: selected_value.sample_values,
+              color: selected_value.otu_ids,
               colorscale: "Rainbow"
           }
       };
@@ -170,17 +162,70 @@ function buildBubbleChart(sample) {
   });
 };
 
-// Function that updates dashboard when sample is changed
-function optionChanged(values) { 
+// Function that builds the gauge chart 
+function buildGaugeChart(sample) {
+
+  // Use D3 to retrieve all of the data
+  d3.json(url).then(data => {
+
+    // Retrieve all metadata
+    let metadata = data.metadata;
+
+    // Filter data where id is equal to the sample 
+    let filtered_data = metadata.filter(result => result.id == sample);
+
+    // Log the array of metadata objects after the have been filtered
+    console.log(filtered_data)
+
+     // Get the first index from the array
+     let selected_value = filtered_data[0]
+
+     // Trace for the data for the gauge chart
+     let trace2 = [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: selected_value.wfreq,
+      title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week", font: {size: 24}},
+      type: "indicator", 
+      mode: "gauge+number",
+      gauge: {
+          axis: {range: [null, 10]}, 
+          bar: {color: "rgb(68,166,198)"},
+          steps: [
+              { range: [0, 1], color: "rgb(233,245,248)" },
+              { range: [1, 2], color: "rgb(218,237,244)" },
+              { range: [2, 3], color: "rgb(203,230,239)" },
+              { range: [3, 4], color: "rgb(188,223,235)" },
+              { range: [4, 5], color: "rgb(173,216,230)" },
+              { range: [5, 6], color: "rgb(158,209,225)" },
+              { range: [6, 7], color: "rgb(143,202,221)" },
+              { range: [7, 8], color: "rgb(128,195,216)" },
+              { range: [8, 9], color: "rgb(113,187,212)" },
+              { range: [9, 10], color: "rgb(98,180,207)" }
+          ]
+      }
+  }];
+    // Set up the Layout
+    let layout = {
+      width: 400, 
+      height: 400,
+      margin: {t: 0, b:0}
+  };
+   // Use Plotly to plot the data in a gauge chart
+   Plotly.newPlot("gauge", trace2,layout);
+});
+};
+
+  // Function that updates dashboard when sample is changed
+  function optionChanged(selected_value) { 
 
   // Log the new value
-  console.log(values); 
+  console.log(selected_value); 
 
   // Call all functions 
-  buildMetadata(values);
-  buildBarChart(values);
-  buildBubbleChart(values);
-  buildGaugeChart(values);
+  buildMetadata(selected_value);
+  buildBarChart(selected_value);
+  buildBubbleChart(selected_value);
+  buildGaugeChart(selected_value);
 };
 
 // Call the initialize function
